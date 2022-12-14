6e917579f4fb03486ffe4e974554619eb6bd1e25
ermsonya <sonya.yermolova2012@mail.ru>
2022-12-14 06:05:11 +0300

    вторая сцена, дофига как много всего

--- Assets/Scripts/Elder/scene2move.cs
+++ Assets/Scripts/Elder/scene2move.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.UI;
 using TMPro;
-
+public class trig
+{
+    public static bool contact = false;
+    public static int n;
+}
 public class scene2move : MonoBehaviour
 {
     public NPC npc;
@@ -16,26 +20,77 @@ public class scene2move : MonoBehaviour
     public TextMeshProUGUI playerResponseQuote2;
     public Button playerResponse;
     public Button playerResponse2;
-    public int n=0;
+  
+    private int k = 0;
     void Start()
     {
   StartCoroutine(Wait());
        
     }
-   
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player"&& trig.n == 2)
+        {
+            trig.contact = true;
+        }
+        if (other.tag == "Player" && trig.n == 5)
+        {
+            trig.contact = true;
+        }
+        if (other.tag == "Player" && trig.n == 7)
+        {
+            trig.contact = true;
+        }
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
-        switch (n) {
+        switch (trig.n) {
             case 1:
         npcDialogue.text = npc.dialogue[0];
   playerResponseQuote.text = npc.playerDialogue[0];
-                n++;
-     
+                StartCoroutine(WaitN());
                 return;
             case 2:
-
-                
+                transform.position = new Vector3(-55 , 2, 94);
+                if (trig.contact == true) {
+                    trig.n++; trig.contact = false;
+                }
+                return;
+            case 3:
+                trig.contact = false;
+             
+                if (k == 0) { dialogueUI.SetActive(true); k++; }
+                npcDialogue.text = npc.dialogue[1];
+                playerResponseQuote.text = npc.playerDialogue[1];   
+                return;
+            case 4:
+                 dialogueUI.SetActive(true);
+                npcDialogue.text = npc.dialogue[2];
+                playerResponseQuote.text = npc.playerDialogue[2];
+                trig.n++; trig.contact = false;
+                return;
+            case 5:
+                transform.position = new Vector3(-111, 0, 80);
+                if (trig.contact == true)
+                {
+                    trig.n++; trig.contact = false;
+                }
+                return;
+            case 6:
+                dialogueUI.SetActive(true); 
+                npcDialogue.text = npc.dialogue[3];
+                playerResponseQuote.text = npc.playerDialogue[3];
+                trig.n++;
+                return;
+            case 7:
+                StartCoroutine(Move()); 
+                if (trig.contact == true)
+                {
+                    npcDialogue.text = npc.dialogue[4];
+                    playerResponseQuote.text = npc.playerDialogue[4];
+                    trig.contact = false; trig.n++;
+                }
                 return;
         }
     }
@@ -47,15 +102,24 @@ public class scene2move : MonoBehaviour
     public void EndDialogue()
     {
         dialogueUI.SetActive(false);
-        playerResponse.interactable = false;
-        playerResponse2.interactable = false;
+      
     }
     public IEnumerator Wait()
 {
     yield return new WaitForSeconds(5);
         npcName.text = npc.Name;
         dialogueUI.SetActive(true);
-        n++;
+        trig.n++;
+    }
+    public IEnumerator WaitN()
+    { 
+        yield return new WaitForSeconds(5);
+        trig.n = 2;
+    }
+    public IEnumerator Move()
+    {
+        yield return new WaitForSeconds(5);
+        transform.position = new Vector3(-135, 0, 122) ;
     }
 
 }
