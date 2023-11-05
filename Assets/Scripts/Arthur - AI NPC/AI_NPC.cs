961d52214d0d7e5bf21651039155681bdd2397fe
Sergei Karpushkin <c1e2r3g@gmail.com>
2023-11-05 22:34:11 +0300

    Доработан скрипт навигации NPC

--- Assets/Scripts/Arthur - AI NPC/AI_NPC.cs
+++ Assets/Scripts/Arthur - AI NPC/AI_NPC.cs
@@ -5,36 +5,21 @@ using UnityEngine.AI;
 
 public class AI_NPC : MonoBehaviour
 {
-    public GameObject target1;
-    public GameObject target2;
-    public GameObject target3;
-    public GameObject target4;
-
-    private List<GameObject> targets = new List<GameObject>();
-    private GameObject randomTarget;
-    private int randomInd;
     private NavMeshAgent agent;
     
     // Start is called before the first frame update
     void Start()
     {
-        targets.Add(target1);
-        targets.Add(target2);
-        targets.Add(target3);
-        targets.Add(target4);
-        
         agent = GetComponent<NavMeshAgent>();
+        agent.destination = AI_NPC_Targets.Instance.GetRandomTarget().position;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (!agent.hasPath)
+        if (agent.remainingDistance < 1f)
         {
-            randomInd = Random.Range(0, targets.Count);
-            randomTarget = targets[randomInd];
-            agent.destination = randomTarget.transform.position;
+            agent.destination = AI_NPC_Targets.Instance.GetRandomTarget().position;
         }
     }
-    
 }
---
+++ Assets/Scripts/Arthur - AI NPC/AI_NPC_Targets.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class AI_NPC_Targets : MonoBehaviour
+{
+    public static List<Transform> targets;
+    public static Transform randomTarget;
+
+    private static int randomInd;
+    
+    public static AI_NPC_Targets Instance;
+
+    private void Awake()
+    {
+        GetTargetsFromParent();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;    
+        
+    }
+    
+    private void GetTargetsFromParent()
+    {
+        targets = new List<Transform>();
+        foreach (Transform child in gameObject.transform)
+        {
+            targets.Add(child.gameObject.transform);
+        }
+    }
+    
+    public Transform GetRandomTarget()
+    {
+        randomInd = Random.Range(0, targets.Count);
+        randomTarget = targets[randomInd];
+        return randomTarget;
+    }
+}
