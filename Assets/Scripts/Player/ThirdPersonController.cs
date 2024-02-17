d9861616fb07af98b8cd1d7f6d94c33d73d831aa
denfranke <33222627+denfranke@users.noreply.github.com>
2024-02-17 19:01:02 +0300

    доработка
    esc закрывает карту, прыжок не сработает после закрытия карты при нажатии во время неё

--- Assets/Scripts/Player/ThirdPersonController.cs
+++ Assets/Scripts/Player/ThirdPersonController.cs
@@ -264,7 +264,8 @@ namespace StarterAssets
 
         private void CheckOtherKeyPressed ()
         {
-            if(_input.map)
+            //Debug.Log(isMapOpen);
+            if(_input.map || (_input.esc && isMapOpen))
             {
                 isMapOpen = !isMapOpen;
                 Cursor.lockState = isMapOpen ? CursorLockMode.None : CursorLockMode.Locked;
@@ -274,6 +275,7 @@ namespace StarterAssets
                 map.SetActive(isMapOpen);
                 _input.map = false;
             }
+            _input.esc = false;
         }
 
         private void Sprint ()
@@ -464,7 +466,7 @@ namespace StarterAssets
 
         private void JumpAndGravity ()
         {
-            if(Grounded)
+            if(Grounded && _input.map==false)
             {
                 // reset the fall timeout timer
                 _fallTimeoutDelta = FallTimeout;
